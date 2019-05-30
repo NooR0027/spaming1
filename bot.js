92ce00623ee5a89d4f2e12de2e50b4d397a81d98
NooR0027 <51165165+NooR0027@users.noreply.github.com>
2019-05-30 13:17:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("578505943013130240")
 setInterval(function() {
-channel.send(`لمالىةالةىلاةالةالةالةالةالةلاةالةالةالةالةالةالةالةالةالةالةالةالةالةالةالةالةلاةالةال`);
+channel.send(`NOoR NOoR NOoR NOoR`);
 }, 30)
 })
 
